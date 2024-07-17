5e177cc7127c4cf4c9b2472368a3e4750483ebe3
talksik <patel.arjun50@gmail.com>
2024-07-16 22:20:19 -0700

    adding in tabs plugin

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -7,17 +7,17 @@ return {
     'nvim-treesitter/nvim-treesitter-context',
     config = function()
       require('treesitter-context').setup {
-        enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
-        max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
-        min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
+        enable = true,            -- Enable this plugin (Can be enabled/disabled later via commands)
+        max_lines = 0,            -- How many lines the window should span. Values <= 0 mean no limit.
+        min_window_height = 0,    -- Minimum editor window height to enable context. Values <= 0 mean no limit.
         line_numbers = true,
         multiline_threshold = 20, -- Maximum number of lines to show for a single context
-        trim_scope = 'outer', -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
-        mode = 'cursor', -- Line used to calculate context. Choices: 'cursor', 'topline'
+        trim_scope = 'outer',     -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
+        mode = 'cursor',          -- Line used to calculate context. Choices: 'cursor', 'topline'
         -- Separator between context and content. Should be a single character string, like '-'.
         -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
         separator = nil,
-        zindex = 20, -- The Z-index of the context window
+        zindex = 20,     -- The Z-index of the context window
         on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
       }
     end,
@@ -94,6 +94,19 @@ return {
     config = function()
       vim.keymap.set('n', '<leader>gg', function()
         vim.cmd 'LazyGit'
+      end, {})
+    end,
+  },
+
+  -- add in shortcuts for next and previous buffer
+  {
+    'romgrk/barbar.nvim',
+    config = function()
+      vim.keymap.set('n', '<leader>9', function()
+        vim.cmd 'BufferPrevious'
+      end, {})
+      vim.keymap.set('n', '<leader>0', function()
+        vim.cmd 'BufferNext'
       end, {})
     end,
   },

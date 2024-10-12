cc2c6063b1712abac683af671933f53d4ffd302f
talksik <patel.arjun50@gmail.com>
2024-10-12 12:35:24 -0700

    taking out annoying tab handling

--- init.lua
+++ init.lua
@@ -246,7 +246,7 @@ vim.opt.rtp:prepend(lazypath)
 -- NOTE: Here is where you install your plugins.
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
-  'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
+  -- 'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
 
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
@@ -589,9 +589,9 @@ require('lazy').setup({
         html = {
           filetypes = { 'html', 'templ' },
         },
-        htmx = {
-          filetypes = { 'htmx', 'templ' },
-        },
+        -- htmx = {
+        --   filetypes = { 'htmx', 'templ' },
+        -- },
         tailwindcss = {
           filetypes = { "templ", "astro", "javascript", "typescript", "react" },
           settings = {

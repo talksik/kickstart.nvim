58c86d04d703f7812e3ae5344d9164d2c7717ad9
talksik <patel.arjun@gmail.com>
2024-11-24 09:42:00 -0800

    disabling weird sql things that conflict with my <C-c> as escape

--- init.lua
+++ init.lua
@@ -90,6 +90,9 @@ P.S. You can delete this when you're done too. It's your config now! :)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
+-- disbable weird sql stuff
+vim.g.omni_sql_no_default_maps = 1
+
 -- Set to true if you have a Nerd Font installed
 vim.g.have_nerd_font = false
 
